2b5d84aeb5ec2bdf9307408482be1aecd262e098
GustavoG0etten <gustavogoetten87@gmail.com>
2024-03-11 22:09:12 -0300

    test

---
+++ projeto-mvc-backend/src/br/com/projetoMVC/util/connectionFactory.java
@@ -0,0 +1,55 @@
+package br.com.projetoMVC.util;
+
+import java.beans.Statement;
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class connectionFactory {
+	
+	public static final String PATH = "jdbc:postgresql://localhost:5432/turmajava";
+	public static final String USER = "postgres";
+	public static final String PASSWORD = "postgres";
+	
+	// Método que retorna a conexão estabelecida 
+	public static Connection getConnection() throws Exception{
+		try {
+			// Carregar o driver do postgresql
+			Class.forName("org.postgresql.Driver");
+			return DriverManager.getConnection(PATH, USER, PASSWORD);
+		} catch (Exception e) {
+			// Mostra uma mensagem de erro caso a conexão falhar
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+		close(conn, stmt, rs);
+	}
+	
+	public static void closeConnection(Connection conn, Statement stmt) {
+		close(conn, stmt, null);
+	}
+	
+	public static void closeConnection(Connection conn) {	
+		close(conn, null, null);
+	}
+	
+	private static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			} 
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+
+}
