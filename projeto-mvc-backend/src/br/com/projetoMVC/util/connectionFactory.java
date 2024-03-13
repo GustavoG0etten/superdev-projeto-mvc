91c1a1530a2e30325a47a8793e63da4308a64b92
GustavoG0etten <gustavogoetten87@gmail.com>
2024-03-13 20:09:55 -0300

    Conexão do Banco de Dados com o Eclipse

--- projeto-mvc-backend/src/br/com/projetoMVC/util/connectionFactory.java
+++ projeto-mvc-backend/src/br/com/projetoMVC/util/connectionFactory.java
@@ -1,6 +1,6 @@
 package br.com.projetoMVC.util;
 
-import java.beans.Statement;
+import java.sql.Statement;
 import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,9 +8,9 @@ import java.sql.ResultSet;
 
 public class connectionFactory {
 	
-	public static final String PATH = "jdbc:postgresql://localhost:5432/turmajava";
+	public static final String PATH = "jdbc:postgresql://localhost:5432/db_superdev";
 	public static final String USER = "postgres";
-	public static final String PASSWORD = "postgres";
+	public static final String PASSWORD = "Mexem301@";
 	
 	// Método que retorna a conexão estabelecida 
 	public static Connection getConnection() throws Exception{
@@ -24,32 +24,34 @@ public class connectionFactory {
 		}
 	}
 	
-	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) throws Exception {
 		close(conn, stmt, rs);
 	}
 	
-	public static void closeConnection(Connection conn, Statement stmt) {
+	public static void closeConnection(Connection conn, Statement stmt) throws Exception {
 		close(conn, stmt, null);
 	}
 	
-	public static void closeConnection(Connection conn) {	
+	public static void closeConnection(Connection conn) throws Exception {	
 		close(conn, null, null);
 	}
 	
-	private static void close(Connection conn, Statement stmt, ResultSet rs) {
+	private static void close(Connection conn, Statement stmt, ResultSet rs) throws Exception {
 		try {
-			if (rs != null) {
+			if (rs != null) 
 				rs.close();
-			}
-			if (stmt != null) {
+			
+			if (stmt != null) 
 				stmt.close();
-			}
-			if (conn != null) {
+				
+			if (conn != null) 
 				conn.close();
-			} 
+			 
 		} catch (Exception e) {
 			throw new Exception(e.getMessage());
 		}
 	}
+	
+	
 
 }
